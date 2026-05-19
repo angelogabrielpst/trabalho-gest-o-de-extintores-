5d8484a771b64b796a46ff71f65f0485ed7358b2
Eduardo Rogério <eduardorogeriodia@gmail.com>
2026-05-19 21:59:29 +0000

    0.1

--- Controller/Extintores.py
+++ Controller/Extintores.py
@@ -1,45 +1,56 @@
-from dataclasses import dataclass
-
+from dataclasses import dataclass, KW_ONLY
+from datetime import date
+#Classe para os setores
 @dataclass
-class extintor:
-       n_cilindro = int
-       n_lacre = int
-       cor_lacre = str
-       classificacao = str
-       agente = str
-       capacidade = int
-       validade = int #transformar em data depois
-       
-       
+class Setor:
+    id_setor: int
+    nome_setor: str
+    bloco_pavimento: str
 
-def inf_extintor(self):
+    def inf_Setor(self):
+        print(f'''
+        ID do Setor: {self.id_setor}
+        Setor: {self.nome_setor}
+        Bloco: {self.bloco_pavimento}
+              ''')
+        
+#Classe que engloba todos os extintores
+@dataclass
+class Extintor:
+    recarregavel: bool
+    numero_patrimonio: int
+    codigo_lacre: int
+    cor_lacre: str
+    classe_incendio: str
+    tipo_agente: str
+    capacidade: int
+    validade_carga: date 
+    
+    #Função pra mostrar o extintor
+    def inf_extintor(self):
         print(f"""
-    Número do Cilindro: {self.n_cilindro}
-    Número do Lacre: {self.lacre}
+    Extintor Recarregável: {self.recarregavel}
+    Número do Cilindro: {self.numero_patrimonio}
+    Número do Lacre: {self.codigo_lacre}
     Cor do lacre: {self.cor_lacre}
-    Classificação: {self.classificacao}
-    Agente: {self.agente}
+    Classificação: {self.classe_incendio}
+    Agente: {self.tipo_agente}
+    Capacidade: {self.capacidade}Kg
+    Validade: {self.validade.strftime}
     """)
        
-
+       #pensando em tirar
+'''
 @dataclass
-class extintor_co2(extintor):
-    tipo: str = 'CO²'
-    pressao: int
-
-
-@dataclass
-class extintor_recarregavel(extintor):
+class ExtintorRecarregavel(Extintor):
+    _: KW_ONLY
     tipo: str = 'Recarregável'
-    data_ult_inspecao:
-    nivel_manut_executado:
+    data_ult_inspecao: date = None
+    nivel_manut_executado: str = 'Nenhum'
 '''
 
-
-'''Print (
-       Número do Cilindro: 
-       Número do Lacre:
-       Cor do lacre:
-       Classificação:
-       Agente:
-)'''
+#inspeções
+@dataclass
+class Inspecoes:
+    id_inspecao: int
+    numero_patrimonio: str
