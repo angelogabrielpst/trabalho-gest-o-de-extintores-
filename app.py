042bbbbe811cd9fcbd37e563fc4970739ec7d4ae
Eduardo Rogério <eduardorogeriodia@gmail.com>
2026-06-20 10:41:09 -0300

    Att

--- app.py
+++ app.py
@@ -1,24 +1,60 @@
 from flask import Flask, request, jsonify
+from twilio.rest import Client
+from dotenv import load_dotenv
 import mysql.connector
+import os
 from datetime import date
+
+load_dotenv()
 
 app = Flask(__name__)
 
+#Twilio
+
+twilio_client = Client(os.getenv('TWILIO_ACCOUNT_SID'), os.getenv('TWILIO_AUTH_TOKEN'))
+
+twilio_client.messages.create(
+    body="Teste de envio",
+    from_=os.getenv("TWILIO_PHONE"),
+    to="+55SEUNUMERO"
+)
+
+twilio_client.messages.create(
+    body="Seu extintor está próximo do vencimento.",
+    from_=os.getenv("TWILIO_WHATSAPP"),
+    to="whatsapp:+55SEUNUMERO"
+)
+
 # Conectar com o Banco de Dados
 db_config = {
-    'host': 'localhost',
-    'database': 'sst_extintores_db',
-    'user': 'root',
-    'password': '0000'
+    'host': os.getenv('DB_HOST'),
+    'database': os.getenv('DB_NAME'),
+    'user': os.getenv('DB_USER'),
+    'password': os.getenv('DB_PASSWORD')
 }
 
 def get_db_connection():
     return mysql.connector.connect(**db_config)
 
+#Funções Twilio
+def enviar_sms(destinatario, mensagem):
+    twilio_client.messages.create(
+        body=mensagem,
+        from_=os.getenv("TWILIO_PHONE"),
+        to=destinatario
+    )
+
+def enviar_whatsapp(numero, mensagem):
+    twilio_client.messages.create(
+        body=mensagem,
+        from_=os.getenv("TWILIO_WHATSAPP"),
+        to=f"whatsapp:{numero}"
+    )
+
  #Funções para fazer o CRUD sem ter que repetir a mesma coisa mil vezes
 def executar_sql(sql, parametros=None):
     conn = get_db_connection()
-    cursor = conn.cursor()
+    cursor = conn.cursor(dictionary=True)
     cursor.execute(sql, parametros)
     conn.commit()
     cursor.close()
@@ -26,7 +62,7 @@ def executar_sql(sql, parametros=None):
     
 def consultar_todos_sql(sql, parametros=None):
     conn = get_db_connection()
-    cursor = conn.cursor()
+    cursor = conn.cursor(dictionary=True)
     cursor.execute(sql, parametros)
     resultados = cursor.fetchall()
     cursor.close()
@@ -35,7 +71,7 @@ def consultar_todos_sql(sql, parametros=None):
     
 def consultar_um_sql(sql, parametros=None):
     conn = get_db_connection()
-    cursor = conn.cursor()
+    cursor = conn.cursor(dictionary=True)
     cursor.execute(sql, parametros)
     resultado = cursor.fetchone()
     cursor.close()
@@ -48,23 +84,33 @@ class setores_loc:
         self.nome_setor = nome_setor
         self.bloco_pavimento = bloco_pavimento
 
+class brigadistas:
+    def __init__(self, nome_brigadista, cpf, telefone, email, whatsapp, data_treinamento, id_setor, id_brigadista=None):
+        self.id_brigadista = id_brigadista
+        self.nome_brigadista = nome_brigadista
+        self.cpf = cpf
+        self.telefone = telefone
+        self.email = email
+        self.whatsapp = whatsapp
+        self.data_treinamento = data_treinamento
+        self.id_setor = id_setor #FK para setores_loc
+
+
 class extintores:
-    def __init__(self, numero_patrimonio, id_setor, codigo_lacre, tipo_agente, classe_incendio, localizacao_detalhada, id_brigadista_responsavel, validade_carga, data_aquisicao, data_ultima_recarga, status, id=None):
-        self.id = id
+    def __init__(self, numero_patrimonio, id_setor, codigo_lacre, tipo_agente, classe_incendio, localizacao_detalhada, validade_carga, data_aquisicao, data_ultima_recarga, extintor_status):
         self.numero_patrimonio = numero_patrimonio
         self.id_setor = id_setor #FK para setores_loc
         self.codigo_lacre = codigo_lacre
         self.tipo_agente = tipo_agente
         self.classe_incendio = classe_incendio
         self.localizacao_detalhada = localizacao_detalhada
-        self.id_brigadista_responsavel = id_brigadista_responsavel #FK Brigadista
         self.validade_carga = validade_carga
-        self.data_aquisicao = data_aquisicao #Adicionar no Sql
-        self.data_ultima_recarga = data_ultima_recarga #Adicionar
-        self.status = status #Adicionar
+        self.data_aquisicao = data_aquisicao
+        self.data_ultima_recarga = data_ultima_recarga
+        self.extintor_status = extintor_status
 
 class inspecoes_extintores:
-    def __init__(self, numero_patrimonio, data_inspecao, status_manometro, status_carga, status_agente_disparo, lacre_rompido, validade_teste_nivel1, validade_teste_nivel2, validade_teste_nivel3, integridade_visual, arquivo_evidencia_imagem_path, id_inspecao=None):
+    def __init__(self, numero_patrimonio, data_inspecao, status_manometro, status_carga, status_agente_disparo, lacre_rompido, data_teste_nivel1, data_teste_nivel2, data_teste_nivel3, data_vencimento_nivel1, data_vencimento_nivel2, data_vencimento_nivel3, integridade_visual, arquivo_evidencia_imagem_path, id_inspecao=None):
         self.id_inspecao = id_inspecao
         self.numero_patrimonio = numero_patrimonio #FK extintores
         self.data_inspecao = data_inspecao
@@ -72,9 +118,12 @@ class inspecoes_extintores:
         self.status_carga = status_carga # Adicionar no Sql
         self.status_agente_disparo = status_agente_disparo
         self.lacre_rompido = lacre_rompido
-        self.validade_teste_nivel1 = validade_teste_nivel1
-        self.validade_teste_nivel2 = validade_teste_nivel2
-        self.validade_teste_nivel3 = validade_teste_nivel3
+        self.data_teste_nivel1 = data_teste_nivel1
+        self.data_teste_nivel2 = data_teste_nivel2
+        self.data_teste_nivel3 = data_teste_nivel3
+        self.data_vencimento_nivel1 = data_vencimento_nivel1
+        self.data_vencimento_nivel2 = data_vencimento_nivel2
+        self.data_vencimento_nivel3 = data_vencimento_nivel3
         self.integridade_visual = integridade_visual
         self.arquivo_evidencia_imagem_path = arquivo_evidencia_imagem_path
 
@@ -138,7 +187,7 @@ def atualizar_setor(id_setor):
         )
     )
 
-    return jsonify({"mensagem": "Setor atualizado"})
+    return jsonify({"mensagem": "Brigadista atualizado"})
 
     #Deletar um setor
 @app.route('/api/setores/<int:id_setor>', methods=['DELETE'])
@@ -147,6 +196,85 @@ def deletar_setor(id_setor):
     executar_sql("DELETE FROM setores_loc WHERE id_setor = %s", (id_setor,))
 
     return jsonify({"mensagem": "Setor deletado"})
+
+#==================Brigadistas=====================================
+
+    #Cadastrar os Brigadistas
+@app.route('/api/brigadistas', methods=['POST'])
+def cadastrar_brigadistas():
+    
+    dados = request.get_json()
+    
+    executar_sql(
+        """
+        INSERT INTO brigadistas
+        (nome_brigadista, cpf, telefone, email, whatsapp, data_treinamento, id_setor)
+        VALUES (%s, %s, %s, %s, %s, %s, %s)
+        """,
+        (
+        dados['nome_brigadista'],
+        dados['cpf'],
+        dados['telefone'],
+        dados['email'],
+        dados['whatsapp'],
+        dados['data_treinamento'],
+        dados['id_setor']
+        )
+    )
+
+    return jsonify({"mensagem": "Brigadista cadastrado"})
+
+    #Listar todos os Brigadistas
+@app.route('/api/brigadistas', methods=['GET'])
+def listar_brigadistas():
+
+    resultado = consultar_todos_sql("SELECT * FROM brigadistas")
+
+    return jsonify(resultado)
+
+    #Listar um Brigadista
+@app.route('/api/brigadistas/<int:id_brigadista>', methods=['GET'])
+def listar_brigadista(id_brigadista):
+    resultado = consultar_um_sql("SELECT * FROM brigadistas WHERE id_brigadista = %s", (id_brigadista,))
+
+    if resultado:
+        return jsonify(resultado)
+    else:
+        return jsonify({"mensagem": "Brigadista não encontrado"}), 404
+
+    #Atualizar um brigadista
+@app.route('/api/brigadistas/<int:id_brigadista>', methods=['PUT'])
+def atualizar_brigadista(id_brigadista):
+
+    dados = request.get_json()
+
+    executar_sql(
+        """
+        UPDATE brigadistas
+        SET nome_brigadista = %s, cpf = %s, telefone = %s, email = %s, whatsapp = %s, data_treinamento = %s, id_setor = %s
+        WHERE id_brigadista = %s
+        """,
+        (
+            dados["nome_brigadista"],
+            dados["cpf"],
+            dados["telefone"],
+            dados["email"],
+            dados["whatsapp"],
+            dados["data_treinamento"],
+            dados["id_setor"],
+            id_brigadista
+        )
+    )
+    
+    return jsonify({"mensagem": "Setor atualizado"})
+
+    #Deletar um brigadista
+@app.route('/api/brigadistas/<int:id_brigadista>', methods=['DELETE'])
+def deletar_brigadista(id_brigadista):
+
+    executar_sql("DELETE FROM brigadistas WHERE id_brigadista = %s", (id_brigadista,))
+
+    return jsonify({"mensagem": "Brigadista deletado"})
 
 #==================Extintores======================================
 
@@ -159,8 +287,8 @@ def cadastrar_extintor():
     executar_sql(
         """
         INSERT INTO extintores
-        (numero_patrimonio, id_setor, codigo_lacre, tipo_agente, classe_incendio, localizacao_detalhada, id_brigadista_responsavel, validade_carga, data_aquisicao, data_ultima_recarga, status)
-        VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+        (numero_patrimonio, id_setor, codigo_lacre, tipo_agente, classe_incendio, localizacao_detalhada, validade_carga, data_aquisicao, data_ultima_recarga, extintor_status)
+        VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
         """,
         (
             dados['numero_patrimonio'],
@@ -169,11 +297,10 @@ def cadastrar_extintor():
             dados['tipo_agente'],
             dados['classe_incendio'],
             dados['localizacao_detalhada'],
-            dados['id_brigadista_responsavel'],
             dados['validade_carga'],
-            dados['data_aquisicao'], #
-            dados['data_ultima_recarga'], #
-            dados['status'] #
+            dados['data_aquisicao'],
+            dados['data_ultima_recarga'],
+            dados['extintor_status']
         )
     )
 
@@ -188,7 +315,7 @@ def listar_extintores():
     return jsonify(resultado)
 
     #Listar um extintor
-@app.route('/api/extintores/<int:numero_patrimonio>', methods=['GET'])
+@app.route('/api/extintores/<string:numero_patrimonio>', methods=['GET'])
 def listar_extintor(numero_patrimonio):
 
     resultado = consultar_um_sql("SELECT * FROM extintores WHERE numero_patrimonio = %s", (numero_patrimonio,))
@@ -199,7 +326,7 @@ def listar_extintor(numero_patrimonio):
         return jsonify({"mensagem": "Extintor não encontrado."}), 404
     
     #Atualizar um extintor
-@app.route('/api/extintores/<int:numero_patrimonio>', methods=['PUT'])
+@app.route('/api/extintores/<string:numero_patrimonio>', methods=['PUT'])
 def atualizar_extintor(numero_patrimonio):
 
     dados = request.get_json()
@@ -207,7 +334,7 @@ def atualizar_extintor(numero_patrimonio):
     executar_sql(
         """
         UPDATE extintores
-        SET id_setor = %s, codigo_lacre = %s, tipo_agente = %s, classe_incendio = %s, localizacao_detalhada = %s, id_brigadista_responsavel = %s, validade_carga = %s, data_aquisicao = %s, data_ultima_recarga = %s, status = %s
+        SET id_setor = %s, codigo_lacre = %s, tipo_agente = %s, classe_incendio = %s, localizacao_detalhada = %s, validade_carga = %s, data_aquisicao = %s, data_ultima_recarga = %s, extintor_status = %s
         WHERE numero_patrimonio = %s
         """,
         (
@@ -216,11 +343,10 @@ def atualizar_extintor(numero_patrimonio):
             dados['tipo_agente'],
             dados['classe_incendio'],
             dados['localizacao_detalhada'],
-            dados['id_brigadista_responsavel'],
             dados['validade_carga'],
             dados['data_aquisicao'], #
             dados['data_ultima_recarga'], #
-            dados['status'], #
+            dados['extintor_status'], #
             numero_patrimonio
         )
     )
@@ -228,7 +354,7 @@ def atualizar_extintor(numero_patrimonio):
     return jsonify({"mensagem": "Extintor atualizado."})
 
     #Deletar um extintor
-@app.route('/api/extintores/<int:numero_patrimonio>', methods=['DELETE'])
+@app.route('/api/extintores/<string:numero_patrimonio>', methods=['DELETE'])
 def deletar_extintor(numero_patrimonio):
 
     executar_sql("DELETE FROM extintores WHERE numero_patrimonio = %s", (numero_patrimonio,))
@@ -246,19 +372,23 @@ def cadastrar_inspecao():
     executar_sql(
         """
         INSERT INTO inspecoes_extintores
-        (numero_patrimonio, data_inspecao, status_manometro, status_carga, status_agente_disparo, lacre_rompido, validade_teste_nivel1, validade_teste_nivel2, validade_teste_nivel3, integridade_visual, arquivo_evidencia_imagem_path)
-        VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+        (id_brigadista, numero_patrimonio, data_inspecao, status_manometro, status_carga, status_agente_disparo, lacre_rompido, data_teste_nivel1, data_teste_nivel2, data_teste_nivel3, data_vencimento_nivel1, data_vencimento_nivel2, data_vencimento_nivel3, integridade_visual, arquivo_evidencia_imagem_path)
+        VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
         """,
         (
+            dados['id_brigadista'],
             dados['numero_patrimonio'],
             dados['data_inspecao'],
             dados['status_manometro'],
             dados['status_carga'], #
             dados['status_agente_disparo'],
             dados['lacre_rompido'],
-            dados['validade_teste_nivel1'],
-            dados['validade_teste_nivel2'],
-            dados['validade_teste_nivel3'],
+            dados['data_teste_nivel1'],
+            dados['data_teste_nivel2'],
+            dados['data_teste_nivel3'],
+            dados['data_vencimento_nivel1'],
+            dados['data_vencimento_nivel2'],
+            dados['data_vencimento_nivel3'],
             dados['integridade_visual'],
             dados['arquivo_evidencia_imagem_path']
         )
@@ -275,7 +405,7 @@ def listar_inspecoes():
     return jsonify(resultado)
 
     #Listar todas as inspeções de um extintor específico
-@app.route('/api/inspecoes/<int:numero_patrimonio>', methods=['GET'])
+@app.route('/api/inspecoes/A/<string:numero_patrimonio>', methods=['GET'])
 def listar_inspecao(numero_patrimonio):
 
     resultado = consultar_um_sql("SELECT * FROM inspecoes_extintores WHERE numero_patrimonio = %s", (numero_patrimonio,))
@@ -285,6 +415,18 @@ def listar_inspecao(numero_patrimonio):
     else:
         return jsonify({"mensagem": "Inspeção não encontrada."}), 404
     
+
+    #Listar inspeção com o brigadista
+@app.route('/api/inspecoes/B/<int:id_brigadista>', methods=['GET'])
+def listar_inspecao_brigadista(id_brigadista):
+
+    resultado = consultar_um_sql("SELECT i.*, b.nome_brigadista FROM inspecoes_extintores i JOIN brigadistas b ON i.id_brigadista = b.id_brigadista WHERE i.id_brigadista = %s", (id_brigadista,))
+
+    if resultado:
+        return jsonify(resultado)
+    else:
+        return jsonify({"mensagem": "Inspeção não encontrada."}), 404
+
     #Atualizar uma inspeção
 @app.route('/api/inspecoes/<int:id_inspecao>', methods=['PUT'])
 def atualizar_inspecao(id_inspecao):
@@ -294,19 +436,24 @@ def atualizar_inspecao(id_inspecao):
     executar_sql(
         """
         UPDATE inspecoes_extintores
-        SET numero_patrimonio = %s, data_inspecao = %s, status_manometro = %s, status_carga = %s, status_agente_disparo = %s, lacre_rompido = %s, validade_teste_nivel1 = %s, validade_teste_nivel2 = %s, validade_teste_nivel3 = %s, integridade_visual = %s, arquivo_evidencia_imagem_path = %s
+        SET id_brigadista = %s, numero_patrimonio = %s, data_inspecao = %s, status_manometro = %s, status_carga = %s, status_agente_disparo = %s, lacre_rompido = %s, data_teste_nivel1 = %s, data_teste_nivel2 = %s, data_teste_nivel3 = %s, data_vencimento_nivel1 = %s,
+        data_vencimento_nivel2 = %s, data_vencimento_nivel3 = %s, integridade_visual = %s, arquivo_evidencia_imagem_path = %s
         WHERE id_inspecao = %s
         """,
         (
+            dados['id_brigadista'],
             dados['numero_patrimonio'],
             dados['data_inspecao'],
             dados['status_manometro'],
             dados['status_carga'],
             dados['status_agente_disparo'],
             dados['lacre_rompido'],
-            dados['validade_teste_nivel1'],
-            dados['validade_teste_nivel2'],
-            dados['validade_teste_nivel3'],
+            dados['data_teste_nivel1'],
+            dados['data_teste_nivel2'],
+            dados['data_teste_nivel3'],
+            dados['data_vencimento_nivel1'],
+            dados['data_vencimento_nivel2'],
+            dados['data_vencimento_nivel3'],
             dados['integridade_visual'],
             dados['arquivo_evidencia_imagem_path'],
             id_inspecao
@@ -326,3 +473,17 @@ def deletar_inspecao(id_inspecao):
 if __name__ == '__main__':
     # Precisa disso aqui pra fazer o debug
     app.run(host='127.0.0.1', port=5000, debug=True)
+
+#Teste
+"""enviar_whatsapp(
+    "+5531999999999",
+    "Seu extintor vence em 30 dias."
+)"""
+
+"""
+mensagem = (
+    f"Olá! O extintor patrimônio {patrimonio} "
+    f"vence em {dias} dias. Manutenção necessária."
+)
+
+enviar_whatsapp(brigadista["telefone"], mensagem)"""
